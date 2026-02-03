419f3669a0f4aba148cb53b18f28f274ce59495a
myshox <myshoxisgood@gmail.com>
2026-02-03 23:28:42 +0800

    還原 lazy 與字型改動，修復 Something went wrong

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import React, { Suspense, lazy } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { StoreProvider } from './context/StoreContext';
 import { LocaleProvider } from './context/LocaleContext';
 import { ToastProvider } from './context/ToastContext';
 import Layout from './components/Layout';
 import PosPage from './pages/PosPage';
+import AdminPage from './pages/AdminPage';
 import AdminGuard from './components/AdminGuard';
 import ErrorBoundary from './components/ErrorBoundary';
-
-const AdminPage = lazy(() => import('./pages/AdminPage'));
 
 function App() {
   return (
@@ -20,7 +19,7 @@ function App() {
               <Layout>
             <Routes>
               <Route path="/" element={<PosPage />} />
-              <Route path="/admin" element={<AdminGuard><Suspense fallback={<div className="p-8 text-center text-stone-500">載入中...</div>}><AdminPage /></Suspense></AdminGuard>} />
+              <Route path="/admin" element={<AdminGuard><AdminPage /></AdminGuard>} />
             </Routes>
               </Layout>
             </BrowserRouter>
